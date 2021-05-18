5f7a05fe8640eb3de94124052f1a8ccaa4e9eb1b
심재욱 <78770230+SimJaeuk@users.noreply.github.com>
2021-05-18 17:34:31 +0900

    Add files via upload

---
+++ Exam_MyBatis_SimpleBBS/src/main/java/com/study/springboot/ExamMyBatisSimpleBbsApplication.java
@@ -0,0 +1,13 @@
+package com.study.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamMyBatisSimpleBbsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamMyBatisSimpleBbsApplication.class, args);
+	}
+
+}
