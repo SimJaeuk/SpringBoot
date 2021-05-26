9b037aa6305d3afdc30e001e7b34893d31b9b603
SimJaeuk <jaehook293@gmail.com>
2021-05-26 17:17:31 +0900

    스프링 데이터 JPA

--- hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,10 +1,10 @@
 package hello.hellospring;
 
-import hello.hellospring.repository.JdbcMemberRepository;
-import hello.hellospring.repository.JdbcTemplateMemberRepository;
-import hello.hellospring.repository.JpaMemberRepository;
-import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,25 +14,36 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private final DataSource dataSource;
-    private final EntityManager em;
+    private final MemberRepository memberRepository;
 
-    public SpringConfig(DataSource dataSource, EntityManager em){
-        this.dataSource = dataSource;
-        this.em = em;
+//    public SpringConfig(MemberRepository memberRepository) {
+//        this.memberRepository = memberRepository;
+//    }
+
+//    private final DataSource dataSource;
+//    private final EntityManager em;
+
+//    public SpringConfig(DataSource dataSource, EntityManager em){
+//        this.dataSource = dataSource;
+//        this.em = em;
+//    }
+
+    //@Autowired는 타입이 일치하는 Bean을 찾아서 연결, 동일한 타입의 bean여러개일 때
+    //@Qualifier를 사용하면 명확하게 의존할 객체를 직접 지정
+
+    public SpringConfig(SpringDataJpaMemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
-
-    @Bean
-    public MemberRepository memberRepository() {
+}
+//    @Bean
+//    public MemberRepository memberRepository() {
 //        return new JdbcMemberRepository(dataSource);
 //        return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+//        return new JpaMemberRepository(em);
+//    }
 
-
-}
